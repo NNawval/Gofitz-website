ba78c25d5d14fd71e82b20b03b089e4e5bac92d4
WillyFFS <willyfrans13@gmail.com>
2023-11-23 16:06:04 +0700

    database

--- controller/index.js
+++ controller/index.js
@@ -13,11 +13,38 @@ let port = 8000;
 
 
 app.get("/",async function(req,res){
-    // let data = await database.getData();
-    // console.log(data);
-    // let data = await database.addData();
-    let data = await database.updateData();
-
+    let date = new Date();
+    let dateTomorrow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+    // let idLapangan = 1;
+    // let input = {
+    //     "date": date,
+    //     "dateTomorrow": dateTomorrow,
+    //     "idLapangan":idLapangan
+    // }
+    // console.log(input);
+    // let data = await database.getKetersediaan(input);
+    console.log(typeof date);
+    console.log(date.year);
+    let a = toString(date).split("+");
+    console.log(typeof a);
+    let c = toString(dateTomorrow).split("+");
+    console.log(a);
+    console.log(c);
+    let b = a[0];
+    let d = c[0] + ("T00:00:00+00:00");
+    console.log(b);
+    console.log(d);
+    b = new Date(b);
+    d = new Date(d);
+    console.log(b);
+    console.log(d);
+    let input = {
+        "date": b,
+        "dateTomorrow": d,
+        "idLapangan":1
+    }
+    let data = await database.getKetersediaan(input);
+    console.log(data);
     res.send(data)
 });
 
