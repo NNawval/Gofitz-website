df30d64436a39b8fcf37c248d383951b74586afd
Pramaditya Fajri Migfar <18221111@std.stei.itb.ac.id>
2023-11-27 15:46:37 +0000

    Update 6 files
    - /controller/config/supabaseClient.js
    - /controller/middleware/middleware.js
    - /controller/routes/route.js
    - /controller/index.js
    - /controller/package-lock.json
    - /controller/package.json

--- controller/config/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-// import { createClient } from '@supabase/supabase-js'
-// const supabaseUrl = process.env.supabaseUrl;
-// const supabaseKey = process.env.supabaseKey;
-// const supabase = createClient({
-//     apiKey: supabaseKey,
-//     project: supabaseUrl
-// });
-
-// export default supabase;
